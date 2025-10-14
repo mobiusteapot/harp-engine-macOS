ca36517cca707d45922f1b758a5daf8310a0e532
Harper Rhett <harperrhett@gmail.com>
2025-10-14 18:59:24 -0500

    less silly extensions

---
+++ HarpEngine/Utilities/FloatExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace HarpEngine.Utilities;
+
+public static class FloatExtensions
+{
+	public static int GetRounded(this float value)
+	{
+		return (int)float.Round(value);
+	}
+
+	public static int GetFloored(this float value)
+	{
+		return (int)float.Floor(value);
+	}
+
+	public static int GetCeiled(this float value)
+	{
+		return (int)float.Ceiling(value);
+	}
+}
