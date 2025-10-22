2628f433802de3109bdfe7b464b52f37dfb2282f
Harper Rhett <harperrhett@gmail.com>
2025-10-22 15:41:36 -0500

    text bindings redone

--- HarpEngine/Graphics/Text.cs
+++ HarpEngine/Graphics/Text.cs
@@ -2,28 +2,22 @@
 
 public static class Text
 {
-	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl)]
-	private static extern void DrawText(string text, int x, int y, int fontSize, Color color);
-	public static void Draw(string text, int x, int y, int fontSize, Color color) => DrawText(text, x, y, fontSize, color);
-	public static void Draw(string text, Vector2 position, int fontSize, Color color) => DrawText(text, (int)float.Round(position.X), (int)float.Round(position.Y), fontSize, color);
+	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "DrawText")]
+	public static extern void Draw(string text, int x, int y, int fontSize, Color color);
+	public static void Draw(string text, Vector2 position, int fontSize, Color color) => Draw(text, (int)float.Round(position.X), (int)float.Round(position.Y), fontSize, color);
 
-	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl)]
-	private static extern void DrawTextEx(Font font, string text, Vector2 position, float fontSize, float spacing, Color tint);
-	public static void Draw(Font font, string text, Vector2 position, float fontSize, float spacing, Color tint) => DrawTextEx(font, text, position, fontSize, spacing, tint);
+	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "DrawTextEx")]
+	public static extern void Draw(Font font, string text, Vector2 position, float fontSize, float spacing, Color tint);
 
-	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl)]
-	private static extern void DrawTextPro(Font font, string text, Vector2 position, Vector2 origin, float rotation, float fontSize, float spacing, Color tint);
-	public static void Draw(Font font, string text, Vector2 position, Vector2 origin, float rotation, float fontSize, float spacing, Color tint) => DrawTextPro(font, text, position, origin, rotation, fontSize, spacing, tint);
+	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "DrawTextPro")]
+	public static extern void Draw(Font font, string text, Vector2 position, Vector2 origin, float rotation, float fontSize, float spacing, Color tint);
 
-	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl)]
-	private static extern void SetTextLineSpacing(int spacing);
-	public static void SetLineSpacing(int spacing) => SetTextLineSpacing(spacing);
+	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "SetTextLineSpacing")]
+	public static extern void SetLineSpacing(int spacing);
 
-	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl)]
-	private static extern int MeasureText(string text, int fontSize);
-	public static int MeasureWidth(string text, int fontSize) => MeasureText(text, fontSize);
+	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "MeasureText")]
+	public static extern int MeasureWidth(string text, int fontSize);
 
-	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl)]
-	private static extern Vector2 MeasureTextEx(Font font, string text, float fontSize, float spacing);
-	public static Vector2 MeasureSize(Font font, string text, float fontSize, float spacing) => MeasureTextEx(font, text, fontSize, spacing);
+	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "MeasureTextEx")]
+	public static extern Vector2 MeasureSize(Font font, string text, float fontSize, float spacing);
 }
